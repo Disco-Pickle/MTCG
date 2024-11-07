5eed745dba296207e70cf86a4c1e2a0419f07aa9
Disco-Pickle <s.sacher102@gmail.com>
2024-11-07 19:07:03 +0100

    User: Credentials, Ressources & Competition methods added, required properties added, refactoring

--- MTCG/UserClass/User.cs
+++ MTCG/UserClass/User.cs
@@ -1,23 +1,72 @@
-﻿namespace MTCG.UserClass
+﻿using MTCG.CardClass;
+
+namespace MTCG.UserClass
 {
     public class User
     {
         #region Constructors
         public User()
         {
-            username = "default";
-            password = "password";
-        }
-        public User(string username, string password)
-        {
-            this.username = username;
-            this.password = password; // Hash here, hash somewhere else?
+            Username = "";
+            Password = "";
+            Stack = [];
+            Coins = 20;
+            ELO = 100;
         }
         #endregion
 
         #region Credentials
-        public string username { get; private set; }
-        public string password { get; private set; }
+        public string Username { get; private set; }
+        public string Password { get; private set; }
+
+        public void Register()
+        {
+            // User is asked for username
+            // User is asked for password
+            // Credentials are sent to server, which saves them
+        }
+        public void Login()
+        {
+            // User is prompted for username
+            // User is prompted for password
+            // Credentials are compared to username-password pairs saved on server
+            // Appropriate response is sent
+        }
+        #endregion
+
+        #region Ressources
+        public Card[] Stack { get; private set; }
+        public Card[] Deck { get; private set; }
+        public int Coins;
+
+        public void AcquireCards()
+        {
+            // Initial set of random cards is put into user's Stack
+        }
+        public void TradeCards(Card givenCard, Card receivedCard) 
+        { 
+            // givenCard is traded for receivedCard
+        }
+        public void BuyPackage()
+        {
+            Coins -= 5;
+            // 5 random cards are added to user's stack
+        }
+        public void BuildDeck()
+        {
+            // User selects 4 cards of their Stack, which are added to Deck
+        }
+        #endregion
+
+        #region Competition
+        public int ELO;
+
+        public void Battle()
+        {
+            // This user and an opponent battle
+            // Opponent is decided by the server
+            // Winnner: +3 ELO, loser: -5 ELO
+        }
         #endregion
     }
 }
