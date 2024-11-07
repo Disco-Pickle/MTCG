b00e543de8b84e819ef69fc50878508ed57f4f04
Disco-Pickle <s.sacher102@gmail.com>
2024-11-07 19:19:07 +0100

    Scoreboard class created

---
+++ MTCG/ScoreboardClass/Scoreboard.cs
@@ -0,0 +1,20 @@
+﻿namespace MTCG.ScoreboardClass
+{
+    internal class Scoreboard
+    {
+        #region Constructors
+        public Scoreboard()
+        {
+        }
+        #endregion
+
+        #region Scoreboard
+        public void DisplayScoreboard()
+        {
+            // Get all user objects from server
+            // Sort them by ELO
+            // Display sorted users and their ELO
+        }
+        #endregion
+    }
+}
