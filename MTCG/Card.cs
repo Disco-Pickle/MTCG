28247bb37a050154deb6ce173fb2ad54644757a8
Disco-Pickle <s.sacher102@gmail.com>
2024-11-07 17:51:37 +0100

    Card class created

---
+++ MTCG/Card.cs
@@ -0,0 +1,26 @@
+﻿namespace MTCG
+{
+    public class Card
+    {
+        #region Constructors
+        public Card()
+        {
+            name = "";
+            damage = 0;
+            element = "";
+        }
+        public Card(string name, int damage, string element)
+        {
+            this.name = name;
+            this.damage = damage;
+            this.element = element;
+        }
+        #endregion
+
+        #region Attributes
+        public string name { get; private set; }
+        public int damage { get; private set; }
+        public string element { get; private set; }
+        #endregion
+    }
+}
--- MTCG/Program.cs
+++ MTCG/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             User user1 = new User("cave_johnson", "i<3aperture");
+            Card card1 = new Card("Turret", 5, "Fire");
         }
     }
 }
